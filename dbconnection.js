a109f2d3dde3a684384f12c3634e502140c1471c
richfergus <richfergus@yahoo.com>
2018-04-28 23:34:32 -0700

    update 2

--- dbconnection.js
+++ dbconnection.js
@@ -1,10 +1,17 @@
 var mysql=require('mysql');
- var connection=mysql.createPool({
- 
-host:'api-project-347754878770:us-central1:asdagggc',
- user:'SA',
- password:'ziggy11!!',
- database:'aggregator'
- 
+ var connection = mysql.createConnection({
+    host:'35.194.2.165',
+    user:'SA',
+    password:'ziggy11!!',
+    database:'aggregator',
+    socketPath: '/cloudsql/projectName:zone:instance-name'
 });
  module.exports=connection;
+
+//  var connection = mysql.createConnection({
+//     host     : 'ip_address',
+//     user     : 'root',
+//     password : 'password',
+//     database : 'database_name'
+//     socketPath: “/cloudsql/projectName:zone:instance-name”
+// });
