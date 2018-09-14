08209a6756bed33f6a9d35554e22b624dc895637
biplavt <biplavt@hawsco.com>
2018-09-14 15:53:07 -0700

    Added get requests in aasf and missions routes

--- routes/missions.route.js
+++ routes/missions.route.js
@@ -1,6 +1,21 @@
 var express = require('express');
 var router = express.Router();
 var missions = require('../models/missions.model');
+var db = require('../dbconnection'); //reference of dbconnection.js
+var moment=require('moment');
+
+
+var calculateDays=function(timeGiven){
+        var end=timeGiven;
+        var now=moment(new Date());
+        //console.log('now.year: ',now.year);
+        //console.log('Current time: ',now);
+        //console.log("matching timestamp: ",end);
+        var duration=moment.duration(now.diff(end));
+        var days=duration.asDays();
+        //console.log('days: ',days);
+        return days;
+}
 router.post('/', function (req, res, next) {
     
     if (req.body.mission_TB != ''){
@@ -17,6 +32,33 @@ router.post('/', function (req, res, next) {
     } else {
         console.log(res.json('done: no items added'));
     }
+}),
+router.get('/', function (req, res, next) {
+    console.log('yippieMission');
+    //var q='SELECT * FROM AASF_TB;';
+    var q='SELECT * FROM mission_TB;';
+    db.query(q, function (error, results, fields) {
+        if (error) throw error;
+            //console.log(results[0].AASFID);
+            //console.log(results);
+            var tempMission=[];
+            for(i=0;i<results.length;i++){
+                console.log(i +"th Date: " + results[i].flightEventStart);
+                var diffDays=calculateDays(results[i].flightEventStart);
+                console.log("diff Days:",diffDays);
+                if(diffDays<5 && diffDays>0)
+                    tempMission.push(results[i]);
+            }
+           // var diffDays=calculateDays(results[0].flightEventStart);
+            //console.log("Diff days:",diffDays);
+
+           res.send(tempMission);
+    });
+    //console.log( db.query('SELECT * FROM AASF_TB;'));
+    // return db.query('INSERT INTO aircraftType_TB SET ?', post, insertCallBack);
+    console.log('yippieMission');
+
+    
 })
 
 module.exports = router;
