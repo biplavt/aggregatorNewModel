46145d5052235b2b7c03574423c57487cfa501fa
biplavt <biplavt@hawsco.com>
2018-09-30 16:03:17 -0700

    Update for ac_tb: Added abstration layer to deal with order of items in objects

--- models/aircraftdetail.model.js
+++ models/aircraftdetail.model.js
@@ -1,20 +1,72 @@
 var db = require('../dbconnection'); //reference of dbconnection.js
+
 function ObjToArray(obj) {
   var arr = obj instanceof Array;
-  return (arr ? obj : Object.keys(obj)).map(function(i) {
-    var val = arr ? i : obj[i];
-    if (typeof val === 'object')
-      return ObjToArray(val);
-    else
-      return val;
+  console.log('arr',arr);
+  var newArray=[];
+  Object.keys(obj).map(function(i) {
+        //console.log('i',i);
+        if (i==='migDate'){
+            newArray[0]=obj[i];
+        }
+        else if(i==='workOrder'){
+            newArray[1]=obj[i];
+        }
+        else if(i==='remarksUpdatedBy'){
+          newArray[2]=obj[i];
+        }
+        else if(i==='pmd_hours'){
+          newArray[3]=obj[i];
+        }
+        else if(i==='remarks'){
+          newArray[4]=obj[i];
+        }
+        else if(i==='MCRate'){
+          newArray[5]=obj[i];
+        }
+        else if(i==='uic'){
+          newArray[6]=obj[i];
+        }
+        else if(i==='status'){
+          newArray[7]=obj[i];
+        }
+        else if(i==='phase_due'){
+          newArray[8]=obj[i];
+        }
+        else if(i==='ei_sn'){
+          newArray[9]=obj[i];
+        }
+        else if(i==='ei_model'){
+          newArray[10]=obj[i];
+        }
+        else if(i==='remarksUpdatedLast'){
+          newArray[11]=obj[i];
+        
+        }else if(i==='coloroverride'){
+          newArray[12]=obj[i];
+        }else{ 
+          console.log("invalid data entry!!")
+            }
+    //   console.log('i',i);
+    // var val = arr ? i : obj[i];
+    // console.log('val:',val);
+    // if (typeof val === 'object')
+    //   return ObjToArray(val);
+    // else
+    //   return val;
   });
+ //console.log('newArray',newArray);
+  return newArray;
 }
 
 var ac = {
   aircraftDetail: function (ac, callback) {
     ac.forEach(element => {
+      //console.log("element:",element);
       let insertValues = ObjToArray(element);
-        var sql = "INSERT INTO ac_TB (migDate,workOrder,remarksUpdatedBy,pmd_hours,remarks,MCRate,uic,status,orgin,phase_due,ei_sn, ei_model,remarksUpdatedLast, coloroverride) VALUES ?";
+     
+      //console.log('insertValues:',insertValues);
+        var sql = "INSERT INTO ac_TB (migDate,workOrder,remarksUpdatedBy,pmd_hours,remarks,MCRate,uic,status,phase_due,ei_sn, ei_model,remarksUpdatedLast, coloroverride) VALUES ?";
         return db.query(sql, [[insertValues]], callback)
     });
   },
--- routes/aircraftdetail.route.js
+++ routes/aircraftdetail.route.js
@@ -1,22 +1,35 @@
 var express = require('express');
 var router = express.Router();
 var ac = require('../models/aircraftdetail.model');
+var db = require('../dbconnection'); //reference of dbconnection.js
+
 router.post('/', function (req, res, next) {
-    let siteID = req.body.siteID
+    //console.log("req.body",req.body.ac_TB);
+    //var tempObj=JSON.parse(req.body);
+    // console.log("tempObj:",tempObj);
+    let siteID = req.body.siteID;
+    //console.log("siteID:",siteID);
     let arrayLength = req.body.ac_TB.length;
-    let counter = 0
+    //console.log("arrayLength:",arrayLength);
+    let counter = 0;
     ac.aircraftDetail(req.body.ac_TB, function (err, result) { // insert ac detail data to Db
         if (err) {
             console.log(err)
         } else {
+            //console.log('********************');
+            //console.log("results:",result);
+            //console.log("**************************");
             for (var OkPacket of Object.keys(result)) {
+                //console.log('OkPacket:',OkPacket);
                     if(OkPacket == 'insertId'){
                         if (result[OkPacket] > 0) { // if ad detail insert went ok.
                             ac.insertJoin(siteID, result[OkPacket], function (err, extra) { // insert to join table
                                 if (err) {
+                                    console.log("$");
                                     console(err);
                                 } else {
-                                    console.log(extra)
+                                    console.log("*");
+                                    console.log(extra);
                                     if (extra.session) {
                                         res.json({session: extra.session});
                                         // res.json('done'); //or return count for 1 &amp;amp;amp; 0
@@ -29,8 +42,37 @@ router.post('/', function (req, res, next) {
         }
         counter++   
         if(counter == arrayLength){
-            res.json('done'); //or return count for 1 &amp;amp;amp; 0
+            res.json('1done'); //or return count for 1 &amp;amp;amp; 0
         }
     }); 
+}),
+router.get('/',function(req,res,next){
+    console.log("aircraft detail get!");
+    var q='SELECT * FROM ac_TB;';
+    db.query(q, function (error, results, fields) {
+        if (error) throw error;
+            var totalMCRate=0;
+            var totalPMDhours=0;
+            var totalPhaseDue=0;
+            console.log(results);
+            for(i=0;i<results.length;i++){
+                //console.log('Results:',results[i].migDate);
+                totalMCRate+=parseInt(results[i].MCRate);
+                totalPMDhours+=parseInt(results[i].pmd_hours);
+                totalPhaseDue+=parseInt(results[i].phase_due);
+            }
+            var avgMCRate=totalMCRate/results.length;
+            var avgPMDHours=totalPMDhours/results.length;
+            res.send({
+                totalMCRate:totalMCRate,
+                avgMCRate:avgMCRate,
+                totalPMDhours:totalPMDhours,
+                avgPMDHours:avgPMDHours,
+                totalPhaseDue:totalPhaseDue
+            })
+            
+    });
 });
+
+
 module.exports = router;
